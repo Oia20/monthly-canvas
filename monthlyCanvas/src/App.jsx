1af3bab4015c22ad16bd00a158673fbc8baebdb0
Jacob <92279567+Oia20@users.noreply.github.com>
2024-04-29 14:59:58 -0500

    added axios dependency, and changed api endpoint.

--- monthlyCanvas/src/App.jsx
+++ monthlyCanvas/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
   const [count, setCount] = useState(0)
-  const [content, setContent] = useState(null); // State to store fetched data
+  const [title, setTitle] = useState(null); // State to store fetched data
   const [image, setImage] = useState(null)
   useEffect(() => {
-    fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/438815')
+    fetch('/api/metmuseum/title') // Assuming your backend has a route '/api/metmuseum/paintings'
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -17,30 +18,13 @@ function App() {
       // Work with the JSON data
       const title = data.title;
       console.log(title);
-      setContent(title); // Set the fetched title into state
+      setTitle(title); // Set the fetched title into state
     })
     .catch(error => {
       // Handle errors
       console.error('There was a problem with the fetch operation:', error);
     });
-      
-      fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/438815')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json(); // Parse the response body as JSON
-      })
-      .then(data => {
-        // Work with the JSON data
-        console.log(data);
-        setImage(data.primaryImage); // Set the fetched data into state
-      })
-      .catch(error => {
-        // Handle errors
-        console.error('There was a problem with the fetch operation:', error);
-      });
-
+  
   }, []); // Empty dependency array to run the effect only once
 
   return (
@@ -55,11 +39,8 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        {content && (
-          <p>{JSON.stringify(content)}</p> // Display the fetched data
-        )}
+        <p>{title}</p>
       </div>
-      <img src={image} />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
