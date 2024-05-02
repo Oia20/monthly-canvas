013819e94caf3d747ae002ed73fab0aea1f0b785
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-02 18:32:43 -0500

    Styling changes, Removed default styling (index.css caused some major problems for a while)

--- client/monthlyCanvas/src/App.jsx
+++ client/monthlyCanvas/src/App.jsx
@@ -1,42 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import Nav from './nav.jsx'
 function App() {
-  const [artist, setArtist] = useState([]);
-  const [title, setTitle] = useState([]);
-  const [image, setImage] = useState([]);
-
+  const [artist, setArtist] = useState('');
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchArtists();
-  }, []); // Dependency added to re-fetch data when supabase client changes
+  }, []);
+
   async function fetchArtists() {
     try {
       const response = await fetch('http://localhost:5102/api/Artworks');
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      const date = new Date();
-      const month = date.getMonth()
       const data = await response.json();
-      console.log(data);
-      setArtist(data[month + 1].artist)
-      setTitle(data[month + 1].title)
-      setImage(data[month + 1].image)
-
+      const date = new Date();
+      const month = date.getMonth();
+      const currentData = data[month + 1];
+      
+      setArtist(currentData.artist);
+      setTitle(currentData.title);
+      setImage(currentData.image);
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error);
+    } finally {
+      setLoading(false);
     }
   }
+
   return (
     <>
-      <div>
-        <h1>{artist}</h1>
-        <h2>{title}</h2>
-        <div className="card">
-          <img src={image}/>
+      {loading ? (
+        <div className='div-ld'>
+        <div className="loader">
+          <img src='886728_1_x.webp'/>
         </div>
-      </div>
+        </div>
+      ) : (
+        <>
+        <Nav />
+        <div className='content'>
+          <h1>{artist}</h1>
+          <h2>{title}</h2>
+          <img src={image} alt="Artwork"/>
+        </div>
+        </>
+      )}
     </>
   );
 }
---
+++ client/monthlyCanvas/src/nav.jsx
@@ -0,0 +1,12 @@
+import './nav.css'
+
+export default function Nav() {
+    return (
+        <nav className="header-nav">
+        <div className="logo"><p>Monthly-Canvas</p></div>
+            <p>Sign in</p>
+        </nav>
+    )
+
+}
+
