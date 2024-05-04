362782347abe78683c1bba60aa6f4f1e6bcf25c0
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-03 19:16:20 -0500

    Basic layout of login screen added.

--- client/monthlyCanvas/src/login.jsx
+++ client/monthlyCanvas/src/login.jsx
@@ -1,7 +1,27 @@
 import "./login.css"
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export default function LoginForm() {
     return (
-        <h1>Hello World!</h1>
+        <div className="login-container">
+            <div className="login-form">
+            <h2>Login</h2>
+            <form>
+                <div className="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required />
+                </div>
+                <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            <p>Don't have an account? <a href="#">Sign up</a></p>
+            <Link to="/">
+                <a>Return to this months artwork</a>
+            </Link>
+            </div>
+        </div>
     )
 }
