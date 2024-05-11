02e6f8b393c5ee31efa071223225775915cf9ee6
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-11 15:23:15 -0500

    Whitespace in main.jsx

--- client/monthlyCanvas/src/main.jsx
+++ client/monthlyCanvas/src/main.jsx
@@ -9,12 +9,13 @@ import RegisterForm from './register.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <React.StrictMode>
+
       <Routes>
 
         <Route path="/monthly-canvas" element={<App />} />
         <Route path="/monthly-canvas/login" element={<LoginForm />} />
         <Route path="/monthly-canvas/Register" element={<RegisterForm />} />
-
+        
       </Routes>
     </React.StrictMode>
   </Router>
